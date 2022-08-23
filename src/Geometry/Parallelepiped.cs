0f1329a1791c8e1c86609944ff0915fe3ac70ef9
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-08-23 13:47:39 -0300

    Testando se o git push ainda funciona

--- src/Geometry/Parallelepiped.cs
+++ src/Geometry/Parallelepiped.cs
@@ -1,4 +1,18 @@
 namespace Geometry;
 public class Parallelepiped
 {
+  public double Width { get; set; }
+  public double Height { get; set; }
+  public double Depth { get; set; }
+
+  public double Volume 
+  { 
+    get { return Width * Height * Depth; }
+  }
+
+  public Rectangle FaceA { 
+    get { new Rectangle(Width, Height) }
+    set; }
+  public Rectangle FaceB { get; set; }
+  public Rectangle FaceC { get; set; }
 }
