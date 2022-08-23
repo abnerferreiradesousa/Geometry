ef3337b37eb53fc1e55b38a311acfa4aca51c98f
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-08-23 17:16:56 -0300

    Adicionando testes complementares.

--- src/Geometry.Test/Parallelepiped.Test.cs
+++ src/Geometry.Test/Parallelepiped.Test.cs
@@ -12,4 +12,27 @@ public class ParallelepipedTest
         instance.Width.Should().Be(width);
         instance.Height.Should().Be(height);
     }
+
+    [InlineData(2, 3, 4, 24)]
+    [InlineData(3, 4, 5, 60)]
+    [Theory]
+    public void TestParallelepipedVolumeCalc
+    (
+        double height, 
+        double width, 
+        double depth,
+        double expectedVolume
+    )
+    {
+        var instance = new Parallelepiped(width, height, depth);
+        instance.Volume.Should().Be(expectedVolume);
+    }
+
+        [InlineData(-1, 4, 2)]
+    [Theory]
+    public void TestParallelepipedNonPositiveSideException(double sideA, double sideB, double sideC)
+    {
+        Action act = () => new Parallelepiped(sideA, sideB, sideC);
+        act.Should().Throw<ArgumentException>().WithMessage("All sides must be greater than zero");
+    }
 }
--- src/Geometry.Test/Square.Test.cs
+++ src/Geometry.Test/Square.Test.cs
@@ -11,7 +11,18 @@ public class SquareTest
         var instance = new Square(side);
         instance.Width.Should().Be(side);
         instance.Height.Should().Be(side);
-        // instance.Area.Should().Be(expectedArea);
-        // instance.Perimeter.Should().Be(expectedPerimeter);
+    }
+
+    [InlineData(2, 4, 8)]
+    [InlineData(3, 9, 12)]
+    [InlineData(4, 16, 16)]
+    [Theory]
+    public void TestSquareCalcAreaAndPerimeter(double side, double expectedArea, double expectedPerimeter)
+    {
+        var instance = new Square(side);
+        instance.Width.Should().Be(side);
+        instance.Height.Should().Be(side);
+        instance.Area.Should().Be(expectedArea);
+        instance.Perimeter.Should().Be(expectedPerimeter);
     }
 }
